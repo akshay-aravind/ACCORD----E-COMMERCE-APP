b52093335d0060ea41260d37e08f7245ef071b3d
akshay-aravind <akshay.dx4@gmail.com>
2022-08-22 11:33:50 +0530

    Fetach products and Error handling

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,55 +1,57 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import users from './data/users.js'
-import products from "./data/products.js";
-import User from "./models/userModel";
-import Product from "./models/productModel.js";
-import Order from "./models/orderModel.js";
-import connectDB from "./config/db";
+import products from './data/products.js'
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
 
 connectDB()
+
 const importData = async () => {
-    try{
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-       const createdUsers = await User.insertMany(users)
+    const createdUsers = await User.insertMany(users)
 
-       const adminUser = createdUsers[0]._id
+    const adminUser = createdUsers[0]._id
 
-       const sampleProducts = products.map(product => {
-        return { ...product, user: adminUser}
-       })
-       await Product.insertMany(sampleProducts)
-       console.log('Data imported!'.green.inverse)
-       process.exit()
-      }catch (error){
-        console.error(`${error}`.red.inverse)
-        process.exit(1)
-      }
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser }
+    })
+
+    await Product.insertMany(sampleProducts)
+
+    console.log('Data imported!'.green.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
 
 const destroyData = async () => {
-    try{
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-     
-       console.log('Data Destroyed!'.red.inverse)
-       process.exit()
-      }catch (error){
-        console.error(`${error}`.red.inverse)
-        process.exit(1)
-      }
+    console.log('Data Destroyed!'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
 
 if (process.argv[2] === '-d') {
-    destroyData()
+  destroyData()
 } else {
-    importData()
+  importData()
 }
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
 import express from 'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 9000
 
 app.get('/', (req, res) => {
   res.send('API is Running')
 })
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
 
-app.listen(port, () => console.log(` ${port} in ${process.env.NODE_ENV} mode `.yellow.bold))
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () =>
+  console.log(` ${port} in ${process.env.NODE_ENV} mode `.yellow.bold)
+)

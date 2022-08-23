a43ef314abf297aefe132d0fa866704582c8e0d5
akshay-aravind <akshay.dx4@gmail.com>
2022-08-23 11:27:19 +0530

    redux state to homescreen

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,22 +6,29 @@ const router = express.Router()
 
 //Fetch all products - Get /api/products
 
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({})
-  res.json(products)
-}))
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+   
+    res.json(products)
+  })
+)
 
 //Fetch single products - Get /api/products/:id
 
-router.get('/:id', asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id)
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
 
-  if(product) {
-    res.json(product)
-  } else{
-    res.status(404)
-    throw new Error('Product not found')
-  }
-}))
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+)
 
 export default router

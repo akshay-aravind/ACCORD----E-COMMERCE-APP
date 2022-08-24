22fa18d8244174357092a597d3f04e7ea1c4f9f3
akshay-aravind <akshay.dx4@gmail.com>
2022-08-24 18:52:07 +0530

    Back end user authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,10 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import { getProductById, getProducts } from '../controllers/productController.js'
 
 const router = express.Router()
 
-//Fetch all products - Get /api/products
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-   
-    res.json(products)
-  })
-)
-
-//Fetch single products - Get /api/products/:id
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
-import express from 'express'
+import express, { application } from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
-
+import userRoutes from './routes/userRoutes.js'
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+app.use(express.json())
 const port = process.env.PORT || 9000
 
 app.get('/', (req, res) => {
@@ -17,6 +19,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)

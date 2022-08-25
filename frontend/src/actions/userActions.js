01777e989f694c6837212a4a553978aa1ba1537b
akshay-aravind <akshay.dx4@gmail.com>
2022-08-25 18:59:16 +0530

    Logging out

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,8 +1,13 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_SUCCESS } from '../constants/userConstants'
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+} from '../constants/userConstants'
+
 import axios from 'axios'
 
 export const login = (email, password) => async (dispatch) => {
-  try {
+   try {
     dispatch({
       type: USER_LOGIN_FAIL,
     })
@@ -18,18 +23,23 @@ export const login = (email, password) => async (dispatch) => {
     )
 
     dispatch({
-        type: USER_LOGIN_SUCCESS,
-        payload : data
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
     })
 
-    localStorage.setItem('userInfo',JSON.stringify(data))
+    localStorage.setItem('userInfo', JSON.stringify(data))
   } catch (error) {
     dispatch({
-        type: USER_LOGIN_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      })
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
   }
+}
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo')
+  dispatch({ type: USER_LOGOUT })
+  document.location.href = '/login'
 }

65bf87b6c724b2b8d87916bc2f807c62c6ac8e91
akshay-aravind <akshay.dx4@gmail.com>
2022-08-27 17:19:31 +0530

    placeorder screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 const App = () => {
   return (
@@ -19,7 +20,8 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Routes>
-          <Route  path='/payment' element={<PaymentScreen/>} />          
+          <Route  path='/payment' element={<PaymentScreen/>} />   
+          <Route  path='/placeorder' element={<PlaceOrderScreen/>} />                 
           <Route  path='/shipping' element={<ShippingScreen />} />
             <Route exact path='/login' element={<LoginScreen />} />
             <Route path='/register' element={<RegisterScreen />} />

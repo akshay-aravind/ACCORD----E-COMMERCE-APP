323d82e2a351d9ade83ddacd9f2670a3e64d1aa3
akshay-aravind <akshay.dx4@gmail.com>
2022-09-04 22:51:44 +0530

    update footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -15,18 +15,6 @@ const Footer = () => {
 
             <div>
               <a href='/' className='me-4 text-reset'>
-                <MDBIcon fab icon='facebook-f' />
-              </a>
-              <a href='/' className='me-4 text-reset'>
-                <MDBIcon fab icon='twitter' />
-              </a>
-              <a href='/' className='me-4 text-reset'>
-                <MDBIcon fab icon='google' />
-              </a>
-              <a href='/' className='me-4 text-reset'>
-                <MDBIcon fab icon='instagram' />
-              </a>
-              <a href='/' className='me-4 text-reset'>
                 <MDBIcon fab icon='linkedin' />
               </a>
               <a href='/' className='me-4 text-reset'>
@@ -40,79 +28,56 @@ const Footer = () => {
               <MDBRow className='mt-3'>
                 <MDBCol md='3' lg='4' xl='3' className='mx-auto mb-4'>
                   <h6 className='text-uppercase fw-bold mb-4'>
-                    <MDBIcon icon='gem' className='me-3' />
-                    Company name
+                   <i class="fab fa-angular "></i>
+                    ACCORD
                   </h6>
                   <p>
-                    Here you can use rows and columns to organize your footer
-                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit.
-                  </p>
+                    Helloo, Thinking of buying a new phone?
+                    Then this is the place,  Here we deal with premium smart phone which are considered to be the best. Maaking Us the best.
+                    </p>
                 </MDBCol>
 
                 <MDBCol md='2' lg='2' xl='2' className='mx-auto mb-4'>
                   <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
                   <p>
                     <a href='#!' className='text-reset'>
-                      Angular
+                      Apple
+                    </a>
+                  </p>
+                  <p>
+                    <a href='#samsung' className='text-reset'>
+                      Samsung
                     </a>
                   </p>
                   <p>
                     <a href='#!' className='text-reset'>
-                      React
+                      Pixel
                     </a>
                   </p>
                   <p>
                     <a href='#!' className='text-reset'>
-                      Vue
-                    </a>
-                  </p>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Laravel
+                      Oneplus
                     </a>
                   </p>
                 </MDBCol>
 
-                <MDBCol md='3' lg='2' xl='2' className='mx-auto mb-4'>
-                  <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Pricing
-                    </a>
-                  </p>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Settings
-                    </a>
-                  </p>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Orders
-                    </a>
-                  </p>
-                  <p>
-                    <a href='#!' className='text-reset'>
-                      Help
-                    </a>
-                  </p>
-                </MDBCol>
+
 
                 <MDBCol md='4' lg='3' xl='3' className='mx-auto mb-md-0 mb-4'>
                   <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
                   <p>
                     <MDBIcon icon='home' className='me-2' />
-                    New York, NY 10012, US
+                    Kochi, Kerala, India
                   </p>
                   <p>
                     <MDBIcon icon='envelope' className='me-3' />
-                    info@example.com
+                    support@accord.com
                   </p>
                   <p>
-                    <MDBIcon icon='phone' className='me-3' /> + 01 234 567 88
+                    <MDBIcon icon='phone' className='me-3' /> + 91 7223 4567 88
                   </p>
                   <p>
-                    <MDBIcon icon='print' className='me-3' /> + 01 234 567 89
+                    <MDBIcon icon='print' className='me-3' /> + 91 7223 4567 89
                   </p>
                 </MDBCol>
               </MDBRow>
@@ -123,9 +88,9 @@ const Footer = () => {
             className='text-center p-4'
             style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
           >
-            © 2021 Copyright:
+            © 2022 Copyright:
             <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-              MDBootstrap.com
+              accord.com
             </a>
           </div>
         </MDBFooter>

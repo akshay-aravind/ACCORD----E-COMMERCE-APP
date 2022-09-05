890050870db442e6b9c68095ba5118fecdd588aa
akshay-aravind <akshay.dx4@gmail.com>
2022-09-05 19:21:23 +0530

    name bug fix

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -16,6 +16,7 @@ import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
 import { listMyOrders } from '../actions/orderActions'
 import { LinkContainer } from 'react-router-bootstrap'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 const ProfileScreen = () => {
   const [name, setName] = useState('')
@@ -43,7 +44,8 @@ const ProfileScreen = () => {
     if (!userInfo) {
       navigate('/login')
     } else {
-      if (!user.name) {
+      if (!user || !user.name || success) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET })
         dispatch(getUserDetails('profile'))
         dispatch(listMyOrders())
       } else {
@@ -51,7 +53,7 @@ const ProfileScreen = () => {
         setEmail(user.email)
       }
     }
-  }, [dispatch, navigate, userInfo, user])
+  }, [dispatch, navigate, userInfo, user,success])
 
   const submitHanlder = (e) => {
     e.preventDefault()
@@ -64,57 +66,56 @@ const ProfileScreen = () => {
   return (
     <Row>
       <Col md={5}>
-          <h1>User Profile</h1>
-          {message && <Message variant='danger'>{message}</Message>}
-          {error && <Message variant='danger'>{error}</Message>}
-          {success && <Message variant='success'>Profile Updated</Message>}
-          {loading && <Loader />}
+        <h1>User Profile</h1>
+        {message && <Message variant='danger'>{message}</Message>}
+        {error && <Message variant='danger'>{error}</Message>}
+        {success && <Message variant='success'>Profile Updated</Message>}
+        {loading && <Loader />}
 
-          <FormGroup controlId='name'>
-            <FormLabel>Name</FormLabel>
+        <FormGroup controlId='name'>
+          <FormLabel>Name</FormLabel>
+          <FormControl
+            type='name'
+            placeholder='Enter name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></FormControl>
+        </FormGroup>
+
+        <Form onSubmit={submitHanlder}>
+          <FormGroup controlId='email'>
+            <FormLabel>Email Address </FormLabel>
             <FormControl
-              type='name'
-              placeholder='Enter name'
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              type='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             ></FormControl>
           </FormGroup>
 
-          <Form onSubmit={submitHanlder}>
-            <FormGroup controlId='email'>
-              <FormLabel>Email Address </FormLabel>
-              <FormControl
-                type='email'
-                placeholder='Enter email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              ></FormControl>
-            </FormGroup>
+          <FormGroup controlId='password'>
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              type='password'
+              placeholder='Enter password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></FormControl>
+          </FormGroup>
+          <FormGroup controlId='confirmPassword'>
+            <FormLabel>Confirm Password</FormLabel>
+            <FormControl
+              type='password'
+              placeholder='Confirm password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></FormControl>
+          </FormGroup>
 
-            <FormGroup controlId='password'>
-              <FormLabel>Password</FormLabel>
-              <FormControl
-                type='password'
-                placeholder='Enter password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              ></FormControl>
-            </FormGroup>
-            <FormGroup controlId='confirmPassword'>
-              <FormLabel>Confirm Password</FormLabel>
-              <FormControl
-                type='password'
-                placeholder='Confirm password'
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              ></FormControl>
-            </FormGroup>
-
-            <Button type='submit' variant='primary' className='my-3'>
-              Update
-            </Button>
-          </Form>
-         
+          <Button type='submit' variant='primary' className='my-3'>
+            Update
+          </Button>
+        </Form>
       </Col>
       <Col md={7}>
         <h2>My Orders</h2>
@@ -136,23 +137,33 @@ const ProfileScreen = () => {
             </thead>
             <tbody>
               {orders.map((order) => (
-                  <tr key={order._id}>
-                    <td>{order._id}</td>
-                    <td>{order.createdAt.substring(0,10)}</td>
-                    <td>{order.totalPrice}</td>
-                    <td>{order.isPaid ? order.paidAt.substring(0,10) : (
-                      <i className='fas fa-times' style={{color: 'red'}}></i>
-                    )}</td>
-                     <td>{order.isDelivered ? order.deliveredAt.substring(0,10) : (
-                      <i className='fas fa-times' style={{color: 'red'}}></i>
-                    )}</td>
-                    <td><LinkContainer to= {`/order/${order._id}`}>
-                      <Button className='btn-sm' variant='light'>DETAILS</Button>
-                      </LinkContainer></td>
-                  </tr>
-
-                )
-              )}
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <i className='fas fa-times' style={{ color: 'red' }}></i>
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.deliveredAt.substring(0, 10)
+                    ) : (
+                      <i className='fas fa-times' style={{ color: 'red' }}></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button className='btn-sm' variant='light'>
+                        DETAILS
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </Table>
         )}

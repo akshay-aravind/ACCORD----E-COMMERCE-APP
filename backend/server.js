d32286bb851a62c62bb9489a82ee38ffeabf7738
akshay-aravind <akshay.dx4@gmail.com>
2022-09-06 21:31:13 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname,'..','frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {

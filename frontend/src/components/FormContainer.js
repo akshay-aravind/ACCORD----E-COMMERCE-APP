ccb78a3f710558d84b6bca54db730a2983879541
akshay-aravind <akshay.dx4@gmail.com>
2022-09-06 21:15:49 +0530

    for deployment

--- frontend/src/components/FormContainer.js
+++ frontend/src/components/FormContainer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 
-const FormContainer = ({children}) => {
+const FormContainer = ({ children }) => {
   return (
     <Container>
-        <Row className='justify-content-md-center'>
-            <Col xs={12} md={6}>
-                {children}
-            </Col>
-        </Row>
+      <Row className='justify-content-md-center'>
+        <Col xs={12} md={6}>
+          {children}
+        </Col>
+      </Row>
     </Container>
   )
 }
